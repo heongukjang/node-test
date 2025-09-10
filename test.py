672b3cae0c4e21348a93b41cddffbc83cf5889f6
heongukjang <223434292+heongukjang@users.noreply.github.com>
2025-09-10 16:11:45 +0900

    2번쨰

--- test.py
+++ test.py
@@ -3,15 +3,23 @@ class Parent:
         self.value = "test"
         print(" Parent __init__ called")
 
-    def test(self):
-        print("parent test")
+    def test(self, value):
+        print(value)
 
 class Child(Parent):
     def __init__(self):
         Parent.__init__(self)
         print("Child __init__ called")
 
+    def test2(self):
+        print("========")
+        print(Parent.test(self, 222))
+
+
 child = Child()
-child.test()
+child.test(1)
 print(child.value)
+child.test2()
+
+
 
